759f57191ce599e3fb2d1b0e92715787c24ee957
andreas <andreasjyrimae@gmail.com>
2017-04-02 15:41:10 +0300

    angular putitatud

--- K3/public/core.js
+++ K3/public/core.js
@@ -2,81 +2,113 @@
  * Created by Koffman on 4/1/2017.
  */
 // public/core.js
-var myAnimal = angular.module('myAnimal', []);
+var myAnimal = angular.module('myAnimal', ['ngRoute'])
+    .config(function($routeProvider, $httpProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'index.html',
+                controller: 'mainController',
+                controllerAs: 'vm'
+            }).when('/animal', {
+                templateUrl: 'animal.html',
+                controller: 'animalController',
+                controllerAs: 'vm'
+            }).when('/sighting', {
+                templateUrl: 'sighting.html',
+                controller: 'sightingController',
+                controllerAs: 'vm'
+            })
+   })
+   .controller('mainController', function($http) {
+        
 
-function mainController($scope, $http) {
-    $scope.formData = {};
+        
+    })
+   .controller('animalController', function($http) {
+        var vm = this;
+        vm.formData = {};
 
-    // when landing on the page, get all animals and show them
-    $http.get('/api/animals')
-        .success(function(data) {
-            $scope.animals = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
-
-    // when submitting the add form, send the text to the node API
-    $scope.createAnimal = function() {
-        $http.post('/api/animals', $scope.formData)
+        // when landing on the page, get all animals and show them
+        $http.get('/api/animals')
             .success(function(data) {
-                console.log("WHYs");
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.animals = data;
+                vm.animals = data;
                 console.log(data);
             })
             .error(function(data) {
                 console.log('Error: ' + data);
             });
-    };
 
-    // delete a animal after checking it
-    $scope.deleteAnimal = function(id) {
-        $http.delete('/api/animals/' + id)
+        // when submitting the add form, send the text to the node API
+        vm.createAnimal = function() {
+            $http.post('/api/animals', vm.formData)
+                .success(function(data) {
+                    console.log("WHYs");
+                    vm.formData = {}; // clear the form so our user is ready to enter another
+                    vm.animals = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        };
+
+        // delete a animal after checking it
+        vm.deleteAnimal = function(id) {
+            $http.delete('/api/animals/' + id)
+                .success(function(data) {
+                    vm.animals = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        };
+   })
+   .controller('sightingController', function($http) {
+        var vm = this;
+        vm.formData = {};
+
+        $http.get('/api/animals')
             .success(function(data) {
-                $scope.animals = data;
+                vm.animals = data;
                 console.log(data);
             })
             .error(function(data) {
                 console.log('Error: ' + data);
             });
-    };
 
-    // when landing on the page, get all animals and show them
-    $http.get('/api/sightings')
-        .success(function(data) {
-            $scope.sightings = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
-
-    // when submitting the add form, send the text to the node API
-    $scope.createSighting = function() {
-        $http.post('/api/sightings', $scope.formData)
+        // when landing on the page, get all animals and show them
+        $http.get('/api/sightings')
             .success(function(data) {
-                console.log("WHYs");
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.sightings = data;
+                vm.sightings = data;
                 console.log(data);
             })
             .error(function(data) {
                 console.log('Error: ' + data);
             });
-    };
 
-    // delete a animal after checking it
-    $scope.deleteSighting = function(id) {
-        $http.delete('/api/sightings/' + id)
-            .success(function(data) {
-                $scope.sightings = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+        // when submitting the add form, send the text to the node API
+        vm.createSighting = function() {
+            $http.post('/api/sightings', vm.formData)
+                .success(function(data) {
+                    console.log("WHYs");
+                    vm.formData = {}; // clear the form so our user is ready to enter another
+                    vm.sightings = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        };
 
-}
+        // delete a animal after checking it
+        vm.deleteSighting = function(id) {
+            $http.delete('/api/sightings/' + id)
+                .success(function(data) {
+                    vm.sightings = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        };
+   });
